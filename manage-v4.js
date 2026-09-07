9a7f9d994f2dff57a81139a92d3fc999239b1f51
sayedmohu <36588962+sayedmohu@users.noreply.github.com>
2026-09-07 17:10:19 +0530

    Update manage-v4.js

--- manage-v4.js
+++ manage-v4.js
@@ -115,7 +115,7 @@ function formatMonth(dateString) {
 }
 
 window.editBill = function(id) {
-  const b = bills.find(x => x.monthly_bills === id);
+  const b = bills.find(x => String(x.monthly_bills) === String(id));
   if (!b) return;
 
   editingId = id;
